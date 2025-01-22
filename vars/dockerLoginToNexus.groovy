9b669c58324fdfac0deb3c481c4314691c721c6c
Trinh Nguyen <trinhnguyen@Trinhs-MacBook-Pro.local>
2025-01-22 14:17:25 -0800

    Update docker login to nexus

--- vars/dockerLoginToNexus.groovy
+++ vars/dockerLoginToNexus.groovy
@@ -1,6 +1,6 @@
 def call() {
   echo 'Logging into Nexus repo ....'
-  withCredentials([usernamePassword(credentialsId: 'nexus-credentials', usernameVariable: 'USER', usernamePassword: 'PWD')]){
+  withCredentials([usernamePassword(credentialsId: 'nexus-credentials', usernameVariable: 'USER', passwordVariable: 'PWD')]){
     sh "echo ${PWD} | docker login -u ${USER} --password-stdin 143.110.228.135:8083"
   }
 }
