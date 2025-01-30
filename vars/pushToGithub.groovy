fd1d74483e7ac75ffd5bb4ad80f8da9798ea78e7
Trinh Nguyen <trinhnguyen@Trinhs-MacBook-Pro.local>
2025-01-30 15:01:25 -0800

    update pushToGithubn

--- vars/pushToGithub.groovy
+++ vars/pushToGithub.groovy
@@ -1,6 +1,6 @@
-def call (){
+def call (String gitUrl, branch){
   withCredentials([usernamePassword(credentialsId: 'github-credentials', usernameVariable: 'USER', passwordVariable: 'PWD')]){
-    sh "git remote set-url origin https://${USER}:${PWD}@github.com/ManhTrinhNguyen/jenkins-exercise.git"
+    sh "git remote set-url origin https://${USER}:${PWD}@${gitUrl}"
 
     sh 'git config --global user.email jenkins@gmail.com'
     sh 'git config --global user.name Jenkins'
@@ -10,6 +10,6 @@ def call (){
 
     sh 'git add .'
     sh 'git commit -m "ci: bump version"'
-    sh 'git push origin HEAD:Using-Shared-Library'
+    sh "git push origin HEAD:${branch}"
   }
 }
